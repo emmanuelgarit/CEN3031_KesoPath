20726350e87b866905c37f5b83dad83d22f93066
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2022-03-19 00:55:34 -0400

    register form design

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Avatar from '@mui/material/Avatar';
-import { green } from '@mui/material/colors';
+import { green, blue } from '@mui/material/colors';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
@@ -13,10 +13,18 @@ import { useNavigate } from "react-router-dom";
 import Link from '@mui/material/Link';
 
 export default function Register() {
+    const paperStyle={padding:20, height:'70vh', width:280, margin:"20px auto"};
+    const avatarStyle={backgroundColor:blue[500]};
+    let navigate = useNavigate();
     return (
-        <div>
-            <Typography variant="h1">Register</Typography>
-        </div> 
-    )
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Typography align="center" variant="h4" sx={{ padding: 2 }}>Register</Typography>
+                <TextField label="Username" placeholder="Enter Username" variant="standard" fullWidth required/>
+                <TextField label="Password" placeholder="Enter Password" variant="standard" type="password" fullWidth required/>
+                <Button type="submit" color="primary" variant="contained" sx={{ marginTop: 2 }} fullWidth>Sign Up</Button>
+            </Paper>
+        </Grid>
+    );
      
 }
