0ade2558843418ad7e1ca4874b62f963001bc87c
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2022-03-21 01:55:46 -0400

    register full stack w/ db

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const routesUrls = require('./routes/routes');
+const cors = require('cors');
 
+dotenv.config()
+
+
+mongoose.connect(process.env.DATABASE_ACCESS, () => {
+    console.log("database connected");
+})
+
+//init routes as middleware
+app.use(express.json());
+app.use(cors());
+app.use('/api', routesUrls);
 app.listen(4000, () => console.log("server is running"))
