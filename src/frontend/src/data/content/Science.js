0afb8036056817c45fc737ef1c9901d13cb23798
Kyle Strout <62024550+KyleStrout@users.noreply.github.com>
2022-04-17 15:46:58 -0400

    results content

--- src/frontend/src/data/content/Science.js
+++ src/frontend/src/data/content/Science.js
@@ -1,5 +1,28 @@
 const Science = `
-Information about Science...
+##### Top Career Paths
+
+* Epidemiologist/Medical Scientist (Median Salary: $75,000)
+
+    * Epidemiologists examine the causes of diseases to prevent them from transmitting and recurring, including investigating the triggers of an infection for a public health agency or collecting blood samples at an outpatient care center. These medical scientists might work in hospitals, laboratories or universities, or for pharmaceutical companies or health insurers.
+
+* Psychologist (Median Salary: $106,000)
+
+    * Psychologists study cognitive, emotional, and social processes and behavior by observing, interpreting, and recording how people relate to one another and to their environments. They use their findings to help improve processes and behaviors.
+
+* Forensic Science Technician (Median Salary: $61,000)
+
+    * Forensic science technician is a blanket name for a number of professionals, who might work in the auspices of forensic science. Crime scene investigators, who gather and document evidence at a crime scene, fall under this forensic science umbrella, as do criminalists, who are scientists who primarily work in a laboratory analyzing the evidence. Professionals in this field can also specialize in forensic entomology, anthropology, engineering and more.
+
+* Biochemist (Median Salary: $94,000)
+
+    * Biochemists study the chemical structure of living things, such as proteins, DNA, and cells. They use their research to help understand the causes of diseases and to develop new medicines.
+
+* Archaeologist (Median Salary: $66,000)
+
+    * At its most basic, archaeology is the study of prehistory and human history through the excavation and analyzation of artifacts. There is some methodical digging involved, but the bulk of an archaeologist's work will be done in the lab, analyzing excavated remains.
+
+
+More info on careers related to science: [https://money.usnews.com/careers/best-jobs/rankings/best-science-jobs](https://money.usnews.com/careers/best-jobs/rankings/best-science-jobs)
 
 
 `;
--- src/frontend/src/data/content/Trades.js
+++ src/frontend/src/data/content/Trades.js
@@ -1,7 +1,31 @@
-const Traders = `
-Information about Trades...
+const Trades = `
+##### Top Career Paths
 
+* Construction Manager (Median Salary: $97,000)
 
+    * One of the highest-paying trade jobs, construction managers supervise public, commercial, residential, and industrial construction projects. Also called project managers and general contractors, they oversee projects from start to end. Their responsibilities include preparing estimates, scheduling subcontractors, responding to problems, and reporting to clients. Construction management requires business, leadership, technical, and communication skills.
+
+* Radiation Therapist (Median Salary: $87,000)
+
+    * Radiation therapists provide radiation therapy to patients with cancer and other diseases. They explain treatment plans, use machines to treat patients, keep treatment records, and monitor patients for adverse reactions. Radiation therapists work in hospitals, physicians' offices, and outpatient care centers. They need technical and interpersonal skills, physical stamina, and close attention to detail.
+
+* Dental Hygienist (Median Salary: $77,000)
+
+    * Working under the supervision of a dentist, dental hygienists provide preventive care and look for dental disease in patients. They use various tools to remove plaque, take x-rays, apply fluoride, and educate patients on oral hygiene. Common employers include private dental offices, community health clinics, and public health facilities.
+
+* Aircraft and Avionics Equipment Mechanic and Technician (Median Salary: $67,000)
+
+    * Aircraft and avionics equipment mechanics and technicians perform maintenance and make repairs on aircraft. Some inspect aircraft according to Federal Aviation Administration (FAA) specifications. Aircraft mechanics find electrical and mechanical problems, repair aircraft components, and use diagnostic equipment to test aircraft parts. Avionics technicians test electronic instruments, install instrument panels, repair components, and keep records.
+
+* Electrician (Median Salary: $57,000)
+
+    * Electricians fix, maintain, and install electrical power, lighting, communication, and control systems in buildings. Trainees typically complete a four- or five-year apprenticeship. Some attend a technical program covering basic electrical information, circuitry, and safety, followed by a shorter apprenticeship.
+
+* Plumber (Median Salary: $51,000)
+
+    * Plumbers install and repair water, gas, and other piping systems in homes, businesses, and factories. They install plumbing fixtures, such as bathtubs and toilets, and appliances, such as dishwashers and water heaters. They clean drains, remove obstructions, and repair or replace broken pipes and fixtures.
+
+More info on careers related to trades: [https://www.accreditedschoolsonline.org/resources/highest-paying-trade-jobs/](https://www.accreditedschoolsonline.org/resources/highest-paying-trade-jobs/) 
 `;
 
-export default Traders;
+export default Trades;
